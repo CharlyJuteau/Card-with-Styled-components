1ced2397a9b2e2f7f120db94ed254ce3733963b7
CharlyJuteau <Charly.juteau4@gmail.com>
2022-11-15 11:34:13 +0100

    clean useless code

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ font-weight:400;
 text-align: center
 `;
 
-const Card = styled.div`
-    `;
 
 const CardBody = styled.div`
     border-radius: 5%;
     width: 18rem;
     overflow: hidden;
     box-shadow: 0px 0px 15px -5px;
+    &:hover {
+      background-color: #105b72c2;
+      color: white;
+      width: 45rem;
+      height: 45rem;
+    }
+    
 
 `;
 
 const Image = styled.img`
 position: relative;
-witdh: 50%;
-height: 50%;
 overflow: hidden;
   height: 8rem;
-left: 20%;
+  left: 20%;
 `;
 
 const Activities = styled.ul`
@@ -37,7 +40,6 @@ text-align: center;
 function App() {
   return (
     <CardBody>
-      <Card>
         <Title>Nantes</Title>
         <Image src="https://www.flycorsair.com/sites/default/files/2022-06/AdobeStock_299134759_Editorial_Use_Only%20Nantes%20compresse.jpg" alt="Nantes" />
         <Activities>
@@ -49,7 +51,6 @@ function App() {
           </li>
         </Activities>
         <Text>Lorem Ipsum</Text>
-      </Card>
     </CardBody>
   );
 }
